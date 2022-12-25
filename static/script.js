b9dbe66041223233061b76edee8e959e1aeec2e7
microsounds <microsounds@users.noreply.github.com>
2022-12-25 01:22:02 -0800

    Added platitude

--- static/script.js
+++ static/script.js
@@ -48,6 +48,7 @@ var platitudes = [
 		'hhhey guess what? Itdst MIKU MONDAY!!!',
 	'synthesized love songs from the future',
 	'music that felt nostalgic when it was new',
+	'you\'re my shining star, my grand illusion',
 	'lurid and indecent memories',
 	'google doesn\'t go this far back',
 	'AI generated lyrical nothings',
