52604376e37620afd955aa9ec8fc37a842f86079
microsounds <microsounds@users.noreply.github.com>
2023-01-02 22:24:44 -0800

    scripts/redirects: XHTML validation, rewrote notice text

--- .scripts/redirects.sh
+++ .scripts/redirects.sh
@@ -10,17 +10,22 @@ IFS='
 while read -r old new; do
 	cat <<- EOF > "${old#*/}"
 		<!DOCTYPE html>
-		<html>
+		<html xmlns="http://www.w3.org/1999/xhtml" lang="en-US" xml:lang="en-US">
 		<head>
 			<title>Redirect</title>
-			<meta http-equiv="refresh" content="2; url=${new}"/>
-			<link rel="canonical" href="${new}"/>
-			<link rel="shortcut icon" type="image/x-icon" href="/static/sky.ico"/>
+			<meta http-equiv="refresh" content="10; url=${new}" />
+			<link rel="canonical" href="${new}" />
+			<link rel="shortcut icon" type="image/x-icon" href="/static/sky.ico" />
 		</head>
 		<body>
-			<h1>Please update your bookmarks.</h1>
-			<p>This page has moved to <a href="${new}">${new}</a>,
-				you will be redirected shortly.</p>
+			<img src="/static/button/badge.png" />
+			<h1>Please update your bookmarks!</h1>
+			<h2>
+				This page has been moved to <a href="${new}">${new}</a>,
+				you will be redirected in 10 seconds.
+			</h2>
+			<p>This redirect page will be kept to prevent link rot for now,
+				but please update your bookmarks with the new URL.</p>
 		</body>
 		</html>
 	EOF
